cccdd1cade4e533929443ca983ba2b513eb0b9f1
exidge <exidge@EXI>
2016-12-27 19:04:29 +0100

    Add project files.

---
+++ EF Code First WPF/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EF_Code_First_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+            Baza.initContext();
+            dataGrid.ItemsSource = Baza.context.Uczestnicy.ToList();
+        }
+
+        private void dataGrid1_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void buttonDodaj_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 show = new Window1(true);
+            show.ShowDialog();
+            if (show.DialogResult.Value == true)
+            {
+                gridItemsRefresh();
+                MessageBox.Show("Użytkownik został dodany.");
+            }
+        }
+
+        private void buttonEdytuj_Click(object sender, RoutedEventArgs e)
+        {
+            Uczestnik selected = (Uczestnik)dataGrid.SelectedItem;
+            try
+            {
+                int selectedID = selected.ID;
+                Window1 show = new Window1(false, selectedID);
+                show.ShowDialog();
+                if (show.DialogResult.Value==true)
+                {
+                    gridItemsRefresh();
+                    MessageBox.Show("Użytkownik został zmieniony.");
+                }
+            }
+            catch(NullReferenceException)
+            {
+
+            }
+        }
+
+        private void buttonUsun_Click(object sender, RoutedEventArgs e)
+        {
+            Uczestnik edd = (Uczestnik)dataGrid.SelectedItem;
+            Usun(edd.ID);
+            System.Threading.Thread.Sleep(100);
+            gridItemsRefresh();
+        }
+
+        private void gridItemsRefresh()
+        {
+            dataGrid.ItemsSource = Baza.context.Uczestnicy.ToList();
+            dataGrid.Items.Refresh();
+        }
+
+        public static void Usun(int id)
+        {
+            var local = Baza.context.Set<Uczestnik>()
+                    .Local
+                    .FirstOrDefault(e => e.ID == id);
+            if (local != null)
+            {
+                Baza.context.Entry(local).State = EntityState.Detached;
+            }
+            Uczestnik doUsuniecia = new Uczestnik() { ID = id };
+            Baza.context.Uczestnicy.Attach(doUsuniecia);
+            Baza.context.Uczestnicy.Remove(doUsuniecia);
+            Baza.context.SaveChanges();
+        }
+    }
+}
---
+++ EF Code First WPF/Window1.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EF_Code_First_WPF
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private bool czyDodac;
+        private int _id;
+        public Window1()
+        {
+            //InitializeComponent();
+        }
+        public Window1(bool add)
+        {
+            InitializeComponent();
+            czyDodac = add;
+            if (add)
+            {
+                buttonExecute.Content = "Dodaj";
+                this.Title = "Dodaj uczestnika";
+            }
+            else
+            {
+                buttonExecute.Content = "Edytuj";
+                this.Title = "Edytuj uczestnika";
+            }
+            
+        }
+        public Window1(bool add, int id)
+        {
+            InitializeComponent();
+            czyDodac = add;
+            this._id = id;
+            buttonExecute.Content = "Edytuj";
+            this.Title = "Edytuj uczestnika";
+            
+        }
+
+        private void WyswietlJednego(int id)
+        {
+            Uczestnik doWyswietlenia = Baza.context.Uczestnicy.First(e => e.ID == id);
+            textBoxImie.Text = doWyswietlenia.Imie;
+            textBoxNazwisko.Text = doWyswietlenia.Nazwisko;
+            checkBoxObiad.IsChecked = doWyswietlenia.takObiad;
+            checkBoxNocleg.IsChecked = doWyswietlenia.takNocleg;
+            checkBoxOplata.IsChecked = doWyswietlenia.takOplata;
+            DateTimePickerPrzyjazd.Value = doWyswietlenia.dataPrzyjazdu;
+            DateTimePickerOdjazd.Value = doWyswietlenia.dataOdjazdu;
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!czyDodac)
+            {
+                WyswietlJednego(_id);
+            }
+        }
+        private void Edytuj(Uczestnik uczestnik)
+        {
+            var local = Baza.context.Set<Uczestnik>()
+                    .Local
+                    .FirstOrDefault(e => e.ID == uczestnik.ID);
+            if (local != null)
+            {
+                Baza.context.Entry(local).State = EntityState.Detached;
+            }
+            var entry = Baza.context.Entry(uczestnik);
+            entry.State = EntityState.Modified;
+            Baza.context.SaveChanges();
+        }
+        private void Dodaj(Uczestnik osoba)
+        {
+            Baza.context.Uczestnicy.Add(osoba);
+            Baza.context.SaveChanges();
+        }
+        private void buttonExecute_Click(object sender, RoutedEventArgs e)
+        {
+            Uczestnik doDodania = new Uczestnik();
+            doDodania.ID = this._id;
+            doDodania.Imie = textBoxImie.Text;
+            doDodania.Nazwisko = textBoxNazwisko.Text;
+            doDodania.takObiad = (bool)checkBoxObiad.IsChecked;
+            doDodania.takNocleg = (bool)checkBoxNocleg.IsChecked;
+            doDodania.takOplata = (bool)checkBoxOplata.IsChecked;
+            doDodania.dataPrzyjazdu = (DateTime)DateTimePickerPrzyjazd.Value;
+            doDodania.dataOdjazdu = (DateTime)DateTimePickerOdjazd.Value;
+            if (!czyDodac)
+            {
+                Edytuj(doDodania);
+            }
+            else
+            {
+                Dodaj(doDodania);
+            }
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
